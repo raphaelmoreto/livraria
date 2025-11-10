fe760c225ab4d464912cd726824de3fc1c71abfe
raphaelmoreto <raphaelqmoreto@gmail.com>
2025-11-09 21:59:11 -0300

    configurado conexão com o banco

---
+++ Livraria.Infrastructure/Connection/DatabaseConnectionSqlServer.cs
@@ -0,0 +1,25 @@
+﻿using Livraria.Infrastructure.Interfaces;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Livraria.Infrastructure.Connection
+{
+    public class DatabaseConnectionSqlServer : IDatabaseConnection
+    {
+        private readonly IConfiguration configuration;
+
+        private readonly string connectionString;
+
+        public DatabaseConnectionSqlServer(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.connectionString = configuration.GetConnectionString("SqlConnection")!;
+        }
+
+        public IDbConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ Livraria.Infrastructure/Interfaces/IDatabaseConnection.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Livraria.Infrastructure.Interfaces
+{
+    public interface IDatabaseConnection
+    {
+        IDbConnection GetConnection();
+    }
+}
